1445686368ecefa27eda2412750a94d1b7fd7fcb
Chris Nantau <Chris.Nantau@Gmail.com>
2022-04-01 23:49:59 -0300

    Change to direction enum
    cleanup of basically everything?
    
    remove all the rest of the different tranlate fns and the assoc funcs
    
    remove files

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # SBD
 
 Making your text block diagrams better.. a little bit.
+
+## Limitations
+
+- Probably isn't that fast
+- Definitely uses more memory than it needs to
+
+## FAQ
+
+- My 50MB diagram takes too long to go through SBD
+  - If you're making 50MB diagrams in crappy text format you have other problems
+
+- This uses more RAM than it needs to
+  - Sure does!
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,71 @@
-use std::io;
+use std::io::Read;
+use bitflags::bitflags;
+use std::time::{Instant, Duration};
+use std::{env};
+use std::{collections::HashMap, io, vec};
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    static ref DASH_LOOKUP: HashMap<DFlags, char> = vec![
+        (DFlags::TOP | DFlags::RIGHT | DFlags::BOTTOM, '├'),
+        (DFlags::TOP | DFlags::LEFT | DFlags::BOTTOM, '┤'),
+        (DFlags::TOP | DFlags::LEFT, '┘'),
+        (DFlags::TOP | DFlags::RIGHT, '└'),
+        (DFlags::LEFT | DFlags::BOTTOM, '┐'),
+        (DFlags::RIGHT | DFlags::BOTTOM, '┌')
+    ]
+    .iter()
+    .copied()
+    .collect();
+}
+
+lazy_static! {
+    static ref PIPE_LOOKUP: HashMap<DFlags, char> = vec![
+        (DFlags::TOP | DFlags::RIGHT | DFlags::BOTTOM, '├'),
+        (DFlags::TOP | DFlags::LEFT | DFlags::BOTTOM, '┤'),
+        (DFlags::TOP | DFlags::LEFT, '┘'),
+        (DFlags::TOP | DFlags::RIGHT, '└'),
+        (DFlags::LEFT | DFlags::BOTTOM, '┐'),
+        (DFlags::RIGHT | DFlags::BOTTOM, '┌'),
+    ]
+    .iter()
+    .copied()
+    .collect();
+}
 
 fn main() {
-    let lines = read_all_lines();
+    let total_time_anchor = Instant::now();
+    let mut lines = read_all_lines_vec();
+    let read_all_time = total_time_anchor.elapsed();
+
     let max_len = lines.iter().map(|x| x.len()).max().unwrap() + 1;
+    let ingest_time = total_time_anchor.elapsed();
+    let now = Instant::now();
 
-    let line_vectors = lines
-        .iter()
-        .map(move |s| s.clone() + &str::repeat(" ", max_len - s.len()))
-        .map(|s| s.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+    for line in lines.iter_mut() {
+        line.resize(max_len, ' ')
+    }
 
-    debug_assert!(line_vectors.iter().all(|x| x.len() == max_len));
+    let intake_time = now.elapsed();
+    debug_assert!(lines.iter().all(|x| x.len() == max_len));
+    let mut lookup_time = Duration::default();
+    let mut jiggy_time = Duration::default();
+    let exec_anchor = Instant::now();
+    let mut new_lines = Vec::new();
+    new_lines.reserve(lines.len());
+    for (index, line) in lines.iter().enumerate() {
 
-    for (index, line) in line_vectors.iter().enumerate() {
         let mut new_line = String::new();
         for (i, c) in line.iter().enumerate() {
+            let now = Instant::now();
             let new_val = if !c.is_whitespace() {
                 let pt = Point { row: index, col: i };
-                let loc = get_location(&line_vectors, pt);
+                let loc = get_location(&lines, pt);
+                let lookup_ts = now.elapsed();
+                lookup_time += lookup_ts;
                 let jiggy = loc.get_jiggy();
+                jiggy_time += now.elapsed() - lookup_ts;
                 // println!("{:?}", loc);
                 // println!("{:?} - jiggy", jiggy);
                 if let Some(v) = jiggy {
@@ -26,21 +73,33 @@ fn main() {
                 } else {
                     '🔥'
                 }
-                
             } else {
                 ' '
             };
 
             new_line.push(new_val);
         }
-
-        println!("|{}|", new_line);
+        new_lines.push(new_line);
+    }
+    let exec_time = exec_anchor.elapsed();
+    let write_anchor = Instant::now();
+    for line in new_lines.iter() {
+        println!("│{}│", line);
     }
 
-    // for line in z {
-    //     println!("Original:")
-    //     println!("|{:?}|", line);
-    // }
+    if let  Ok(x) = env::var("SBD_DIAG"){
+        if x != "true" {
+            return;
+        }
+        println!("Write time: {}ms", write_anchor.elapsed().as_millis());
+        println!("Ingest time {}ms", ingest_time.as_millis());
+        println!("Total time to prep inputs: {}ms", intake_time.as_millis());
+        println!("Time in exec loop {}ms", exec_time.as_millis());
+        println!("Lookup Time = {}ms", lookup_time.as_millis());
+        println!("Jiggy Time = {}ms", jiggy_time.as_millis());
+        println!("Total time = {}ms", total_time_anchor.elapsed().as_millis());
+        println!("Read time: {}ms", read_all_time.as_millis());
+    }
 }
 
 fn get_location(vec: &Vec<Vec<char>>, anchor: Point) -> Location {
@@ -49,54 +108,51 @@ fn get_location(vec: &Vec<Vec<char>>, anchor: Point) -> Location {
     let family = kidnap(vec, &anchor);
 
     return Location {
-       // position: Point { ..anchor },
-        value: family[2],
+        value: vec[anchor.row][anchor.col],
         family,
     };
 }
 
 fn kidnap(vec: &Vec<Vec<char>>, anchor: &Point) -> Vec<char> {
     let mut family = Vec::new();
-    
+
     if anchor.row != 0 {
         family.push(vec[anchor.row - 1][anchor.col]); //top
-    }
-    else {
+    } else {
         family.push(' ')
     }
 
-    family.push(vec[anchor.row][anchor.col -1]); //left
-    family.push(vec[anchor.row][anchor.col]); //value
-    family.push(vec[anchor.row][anchor.col +1]); //right
+    family.push(vec[anchor.row][anchor.col - 1]); //left
+    family.push(vec[anchor.row][anchor.col + 1]); //right
 
     if anchor.row != vec.len() - 1 {
         family.push(vec[anchor.row + 1][anchor.col]); //btm
-    }
-    else {
+    } else {
         family.push(' ')
     }
 
-    return  family;
+    return family;
 }
 
-fn read_all_lines() -> Vec<String> {
+fn read_all_lines_vec() -> Vec<Vec<char>> {
     let mut buf = String::new();
     buf.reserve(4096);
     let stdin = io::stdin();
 
-    let mut vec = Vec::<String>::new();
+    let now = Instant::now();
+    _ = stdin.lock().read_to_string(&mut buf);
 
-    while let Ok(_x @ 1..) = stdin.read_line(&mut buf) {
-        vec.push(format!(" {}", buf.trim_end()));
-        buf.clear();
-    }
-
-    return vec;
+    let read_time = now.elapsed();
+    let v =buf.split('\n').map(|s| { format!(" {} ",s).chars().collect()}).collect::<Vec<Vec<char>>>();
+    println!("Time spent reading {}ms", read_time.as_millis());
+    println!("Time spend splitting: {}ms", (now.elapsed() - read_time).as_millis());
+    v
 }
+
 
 #[derive(Debug)]
 struct Location {
-   // position: Point,
+    // position: Point,
     value: char,
     family: Vec<char>,
 }
@@ -114,47 +170,173 @@ trait IntoJiggy {
 impl Location {
     fn get_jiggy(&self) -> Option<char> {
         match self.value {
-            '-' => translate_dashy(self),
-            '|' => translate_pipe(self),
+            '-' => translate_dashy_lookup(self),
+            '|' => translate_pipey_lookup(self),
             '>' | '<' | '^' | 'v' => translate_pointy(self),
-            //'"' => Some('ʺ'), //term output or w/e 
-            x  => Some(x),
+            x => Some(x),
         }
     }
 }
 
-fn translate_dashy(loc: &Location) -> Option<char> {
-    match loc.family[..] {
-        [' ', '-', '-', ' ', '|'] => Some('┐'),
-        ['|', '-', '-', ' ', ' '] => Some('┘'),
-        [' ', ' ', '-', '-', '|'] => Some('┌'),
-        ['|', ' ', '-', '-', ' '] => Some('└'),
-        [z, x, '-', y, '|']  if !x.is_whitespace() && !y.is_whitespace() && (z.is_whitespace() || z == '-')  => Some('┬'),
-        ['|', x, '-', y, z]  if !x.is_whitespace() && !y.is_whitespace() && (z.is_whitespace() || z == '-') => Some('┴'),
-        ['|', '-', '-', '-', '|'] => Some('┼'),
-        _ => Some('─')
+fn connecty_bits_flags(loc: &Location) -> DFlags {
+    loc.family
+        .iter()
+        .enumerate()
+        .map(|(i, v)| {
+            let dirn = match i {
+                //CN: Directions to center
+                0 => DFlags::BOTTOM,
+                1 => DFlags::RIGHT,
+                2 => DFlags::LEFT,
+                3 => DFlags::TOP,
+                _ => unreachable!(),
+            };
+
+            //  if loc.value == '|'{
+            //     println!("Target: {} to the {:?} of {} -- Can connect from: {}, should bridge? {}", v, dirn.opposite(), loc.value,v.can_connect_from(dirn),v.should_bridge_from(dirn) || loc.value.should_bridge_from(dirn.opposite()));
+            //  }
+            let as_direction = dirn.into();
+            if v.can_connect_from(as_direction)
+                && (v.should_bridge_from(as_direction)
+                    || loc.value.should_bridge_from(as_direction.opposite()))
+            {
+                Some(as_direction.opposite().into())
+            } else {
+                None
+            }
+        })
+        .filter_map(|x| x)
+        .fold(DFlags::default(), |x, v| x | v)
+}
+
+bitflags! {
+    #[derive(Default)]
+    struct DFlags : u8 {
+        const TOP    = 0b0001;
+        const LEFT   = 0b0010;
+        const RIGHT  = 0b0100;
+        const BOTTOM = 0b1000;
+    }
+}
+
+fn translate_dashy_lookup(loc: &Location) -> Option<char> {
+    match DASH_LOOKUP.get(&connecty_bits_flags(loc)) {
+        Some(x) => Some(*x),
+        _ => Some('─'),
+    }
+}
+
+fn translate_pipey_lookup(loc: &Location) -> Option<char> {
+    match PIPE_LOOKUP.get(&connecty_bits_flags(loc)) {
+        Some(x) => Some(*x),
+        _ => Some('│'),
     }
 }
 
 fn translate_pointy(loc: &Location) -> Option<char> {
-    match loc.family[..]{
-        [_,'-','>',..] => Some('►'),
-        [..,'^',_,'|'] => Some('▲'),
-        [..,'<','-',_] => Some('►'),
-        ['|',_,'v',..] => Some('►'),
-        _ => None
+    match loc.family[..] {
+        [_, '-', ..] => Some('►'),
+        [.., '|'] => Some('▲'),
+        [.., '-', _] => Some('►'),
+        ['|', ..] => Some('▼'), //Robert
+        _ => None,
     }
-    
+
     //Some('►')
 }
 
-fn translate_pipe(loc: &Location) -> Option<char> {
-    match loc.family[..] {
-        [ _ , ' ', '|', '-', y ] if !y.is_whitespace()=> Some('├'),
-        [ ' ' , ' ', '|', '-', y] if y.is_whitespace() => Some('┘'),
-        [ y , '-', '|', ' ', x ] if !x.is_whitespace() && !y.is_whitespace()=> Some('┤'),
-        [ y , '-', '|', ' ', ' '] if !y.is_whitespace() => Some('┘'),
-        [' ' , '-', '|', ' ', y] if !y.is_whitespace() => Some('┐'),
-        _ => Some('│')
+#[derive(PartialEq, Eq, Clone, Copy, Debug, PartialOrd, Ord)]
+enum Direction {
+    Top,
+    Left,
+    Right,
+    Bottom,
+}
+
+impl Direction {
+    fn opposite(&self) -> Direction {
+        match self {
+            Direction::Top => Self::Bottom,
+            Direction::Left => Self::Right,
+            Direction::Bottom => Self::Top,
+            Direction::Right => Self::Left,
+        }
+    }
+}
+
+impl Into<DFlags> for Direction {
+    fn into(self) -> DFlags {
+        match self {
+            Direction::Top => DFlags::TOP,
+            Direction::Left => DFlags::LEFT,
+            Direction::Right => DFlags::RIGHT,
+            Direction::Bottom => DFlags::BOTTOM,
+        }
+    }
+}
+
+impl Into<Direction> for DFlags {
+    fn into(self) -> Direction {
+        match self {
+            DFlags::TOP => Direction::Top,
+            DFlags::LEFT => Direction::Left,
+            DFlags::RIGHT => Direction::Right,
+            DFlags::BOTTOM => Direction::Bottom,
+            _ => unreachable!(),
+        }
+    }
+}
+
+trait Connectable {
+    fn is_connectable(&self) -> bool;
+    fn can_connect_from(&self, dirn: Direction) -> bool;
+    fn should_bridge_from(&self, dirn: Direction) -> bool;
+}
+
+trait ConnectableFlags {
+    fn is_connectable(&self) -> bool;
+    fn can_connect_from(&self, dirn: DFlags) -> bool;
+    fn should_bridge_from(&self, dirn: DFlags) -> bool;
+}
+
+//The concept of creating a branch is different than terminating a branch.
+//     |
+//  ----Thing
+// makes sense that there would be a T-junc before Thing but
+//   BBBBBBBBBBBBBBB    BBBBBBBBBBBBBBB
+//   --------------- != ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+//   AAAAAAAAAAAAAAA    AAAAAAAAAAAAAAA
+//
+//
+// should_bridge?
+// letters -> Never
+// pipes => top + bottom
+// dashes => left and right
+// pointies => never
+
+impl Connectable for char {
+    fn is_connectable(&self) -> bool {
+        return !self.is_whitespace();
+    }
+
+    fn can_connect_from(&self, dirn: Direction) -> bool {
+        match self {
+            '-' => true,
+            '|' => true,
+            '>' => dirn == Direction::Left,
+            '<' => dirn == Direction::Right,
+            '^' => dirn == Direction::Bottom,
+            'v' => dirn == Direction::Bottom,
+            x if x.is_whitespace() => false,
+            _ => true,
+        }
+    }
+
+    fn should_bridge_from(&self, dirn: Direction) -> bool {
+        match self {
+            '-' => dirn == Direction::Left || dirn == Direction::Right,
+            '|' => dirn == Direction::Top || dirn == Direction::Bottom,
+            _ => false,
+        }
     }
 }
